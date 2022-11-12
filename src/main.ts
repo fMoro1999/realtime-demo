a47610d0126c119b15a851b81383449152f46309
Francesco Moro <f.moro@casale.ch>
2022-11-12 15:39:07 +0100

    Re-added zone

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    ngZone: 'noop'
-  })
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
